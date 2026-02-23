3d81ab8460cbde58838f9b46970d193cb970f6bc
ambady-jayachandran <ambadyjayachandran1@gmail.com>
2026-02-23 22:20:07 +0530

    project name changed

--- Corestore/asgi.py
+++ Corestore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Croestore project.
+ASGI config for Corestore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Croestore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Corestore.settings')
 
 application = get_asgi_application()
