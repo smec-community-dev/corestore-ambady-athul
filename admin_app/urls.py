d811accb5c109c2acb51294a04546bcba535ebda
Athul <athulbnair04@gmail.com>
2026-03-24 11:33:21 +0530

    first admin

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+     path("login/",views.adminlogin),
+    path("adminhome/",views.adminhome),
 ]
--- admin_app/views.py
+++ admin_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib import messages
 
 # Create your views here.
+def adminlogin(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        data=authenticate(request,username=username,password=password)
+        if data:
+            if data.role =="ADMIN":
+                login(request,data)
+                return redirect("/adminhome/")
+                
+            else:
+                messages.error(request,"invalid username or password")
+        else:
+            return redirect("selleregis")    
+    return render(request,"admin-templates/adminlogin.html")
+
+
+def adminhome(request):
+    return render(request,"admin-templates/adminhome.html")
